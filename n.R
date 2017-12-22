15134a8c82013411df0c12caa0da4f49e9846dea
liyang <liyang@emr-header-1.cluster-39263>
2017-12-22 13:36:56 +0800

    yun

--- n.R
+++ n.R
@@ -3,8 +3,9 @@ library(rpart.plot)
 library(randomForest)
 library(adabag)
 library(MASS)
+library(methods)
 
-setwd('/Users/niepeiyun/Documents/rrrf')
+setwd('/home/liyang/niepeiyun/rrrf')
 source('predict_rf2.R')
 source('rf.R')
 source('rf_blb_n.R')
@@ -13,7 +14,9 @@ source('rf_blb_np.R')
 
 ###########生成数据并测试##############
 
-data_num = 150000
+for (k in 1:10){
+print(paste('这是数据量',100000*k,'次的结果'))
+data_num = 150000*k
 #var_num=1000
 a=Sys.time()
 # data0<- data_corr(data_num,1,var_num,0.5)
@@ -54,24 +57,27 @@ time_blb=0
 index_rf=0
 time_rf=0
 
-index=rf(data.train,data.test ,M)
+#index=rf(data.train,data.test ,M)
 
 
 # 
 # index=blb_rf_np(data.train,data.test ,bag_num ,M)
 # index=rf(data.train,data.test,M )
+index=c()
+time=c()
+
 index_all_blb=c()
 time_all_blb=c()
 
+bag_num_vec<-c(10,15,20,25,30,35,40,45,50,60,70,80)
 
-for (i in 1:10){
+for (i in 1:length(bag_num_vec)){
   index_blb=0
   time_blb=0
-  for(j in 1:2){
+  for(j in 1:100){
   
-  print(paste('BLB_RF----------------------------------------------',i))
-  M=100*i
-  bag_num<-10*i
+  print(paste('BLB_RF--bag_num---------------------------------------',i))
+  bag_num<-bag_num_vec[i]
   index=blb_rf_n(data.train,data.test ,bag_num ,M)
   index_blb=index[[1]]+index_blb
   time_blb=index[[2]]+time_blb
@@ -83,15 +89,21 @@ for (i in 1:10){
   
   # time_all=c(time_all,as.numeric(time_rf)/20)
   # index_all=rbind(index_all,index_rf/20)
+  print(index_blb)
+print(time_blb)
   }
-  time_all_blb=c(time_all_blb,as.numeric(time_blb)/2)
-  index_all_blb=rbind(index_all_blb,index_blb/2)
+  time_all_blb=c(time_all_blb,as.numeric(time_blb)/100)
+  index_all_blb=rbind(index_all_blb,index_blb/100)
   
 }
+index=rbind(index,index_all_blb)
+time=c(time,time_all_blb)
+write.csv(index,'/home/liyang/niepeiyun/result/bag_index.csv')
+write.csv(time,'/home/liyang/niepeiyun/result/bag_time.csv')
 
 # index=blb_rf_imb(data.train,data.test,5,M/5) 
 # print(index)
 # index=blb_rf(data.train,data.test,5,M/5)
 # print(index)
-
+}
 
